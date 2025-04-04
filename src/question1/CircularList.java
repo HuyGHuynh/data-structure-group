5834017eba9ebec94cdbe645e06b02acfca2d70f
N-Abbott <nabbott2005@gmail.com>
2025-04-04 17:34:45 -0400

    main CircularList class

--- src/question1/CircularList.java
+++ src/question1/CircularList.java
@@ -1,58 +1,144 @@
-package question1;
+//package Question1;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class CircularList<E> implements Iterable<E> {
-	private int current;
-	private int size;
-	
-	public CircularList() {
-		
-	}
-	
-	public void add(E item) {
-		
-	}
-	
-	public E get() {
-		return null;
-		
-	}
-	
-	public int size() {
-		return 0;
-	}
-	
-	public Iterator<E> iterator() {
-		return null;
-	}
-	
-	private class Node<E> {
-		E data;
-		Node next;
-		
-		public Node(E item) {
-			
-		}
-	}
-	
-	private class CircIterator implements Iterator<E> {
-		Node<E> next;
-		Node<E> previous;
-		boolean nextCalled;
-		
-		public CircIterator() {
-			
-		}
-		
-		public boolean hasNext() {
-			return false;
-		}
-		
-		public E next() {
-			return null;
-		}
-		
-		public void remove() {
+    private Node<E> current;
+    private int size;
 
-		}
-	}
+    public CircularList() {
+        this.current = null;
+        this.size = 0;
+    }
+
+    public void add(E item) {
+        Node<E> newNode = new Node<>(item);
+
+        if (size == 0) {
+            current = newNode;
+            current.next = current;
+        } else {
+            Node<E> temp = current;
+            while (temp.next != current) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.next = current; 
+            current = newNode; 
+        }
+
+        size++;
+    }
+
+    public E get() {
+        if (size == 0) {
+            throw new NoSuchElementException("List is empty");
+        }
+        return current.data;
+    }
+
+    public int size() {
+        return size;
+    }
+    
+    public Iterator<E> iterator() {
+    	CircIterator iterator = new CircIterator();
+        return iterator;
+    }
+
+    private class Node<E> {
+        E data;
+        Node<E> next;
+
+        public Node(E item) {
+            this.data = item;
+            this.next = null;
+        }
+    }
+
+    private class CircIterator implements Iterator<E> {
+        Node<E> next;
+        Node<E> previous;
+        boolean nextCalled;
+
+        public CircIterator() {
+            this.next = current; 
+            this.previous = null;
+            this.nextCalled = false;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return size > 0;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            E data = next.data;
+            previous = next;
+            next = next.next;
+            nextCalled = true;
+            return data;
+        }
+
+        @Override
+        public void remove() {
+            if (!nextCalled) {
+                throw new IllegalStateException("next() must be called before remove()");
+            }
+
+            if (size == 1) {
+                current = null;
+            } else {
+                Node<E> temp = current;
+                while (temp.next != previous) {
+                    temp = temp.next;
+                }
+
+                temp.next = next;
+                if (previous == current) {
+                    current = next;
+                }
+            }
+
+            size--;
+            nextCalled = false;
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        CircularList<Integer> list = new CircularList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println("List size: " + list.size());  // Should print 3
+        System.out.println("Current: " + list.get());  // Should print 3
+
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        it = list.iterator();  
+        while (it.hasNext()) {
+            int value = it.next();
+            if (value == 2) {
+                it.remove();  
+            }
+        }
+
+        System.out.println("List size after removal: " + list.size());  // Should print 2
+        it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    
+    }
 }
