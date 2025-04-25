6e9200422d27cd9dac2b37cad4866734b9c17378
N-Abbott <nabbott2005@gmail.com>
2025-04-24 23:04:45 -0400

    Completed Word Class and Methods

--- src/question2/IndexTree.java
+++ src/question2/IndexTree.java
@@ -15,7 +15,7 @@ public class IndexTree {
 	
 	public void printIndex() {
 		
-	}
+	} 
 	
 	private class Word implements Comparable<Word> {
 		private String w;
@@ -23,19 +23,41 @@ public class IndexTree {
 		private ArrayList<Integer> lines;
 		
 		public Word(String w, int line) {
-			
+			this.w = w;
+			count = 1;
+			lines.add(line);
 		}
 		
 		void addOccurrence(int line) {
-			
+			boolean duplicateLine = false;
+			for(int i = 0; i < lines.size(); i ++) {
+				if(lines.get(i) == line) {
+					duplicateLine = true;
+				}
+			}
+			if(!duplicateLine) {
+				lines.add(line);
+			}
+			count++;
 		}
 		
 		public String toString() {
-			return "";
+			String toString = w + " (" + count + "): ";
+			StringBuilder sb = new StringBuilder(toString);
+
+			for(int i = 0; i < lines.size(); i++) {
+				sb.append(lines.get(i));
+				if(i < lines.size() - 1) {
+					sb.append(", ");
+				}
+			}
+			
+			return toString;
 		}
 		
 		public int compareTo(Word o) {
-			return 0;
+			int compareWord = w.compareTo(o.w);
+			return compareWord;
 		}
 	}
 }
